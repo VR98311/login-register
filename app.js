dfd67d9e3436f875533ff35eba604058554b6260
VR98311 <vikyathrao311@gmail.com>
2023-06-01 10:24:17 +0530

    added mongoose encryption

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require('body-parser')
 const ejs = require("ejs");
+const md5 = require("md5");
+const encrypt = require("mongoose-encryption");
+require("dotenv").config();
 
 const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
@@ -10,19 +13,52 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+const userSchema = new mongoose.Schema({
+    username : String,
+    password : String
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
+const userList = mongoose.model("userList",userSchema);
+
 app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/login", (req, res) => {
+app.route("/login")
+.get((req, res) => {
     res.render("login");
+}).post((req,res)=>{
+   userList.findOne({username:req.body.username}).then((foundUser)=>{
+     if(foundUser.password === req.body.password){
+        res.render("secrets");
+     }
+   });
 });
-app.get("/register", (req, res) => {
+
+app.route("/register")
+.get((req, res) => {
     res.render("register");
+})
+.post((req,res) => {
+    const user = new userList({
+     username: req.body.username,
+     password: (req.body.password)
+    });
+    user.save().then((foundUser)=>{
+        res.render("secrets");
+    }).catch((err)=>{
+        console.log(err);
+    });
 });
+
 app.get("/secrets", (req, res) => {
     res.render("secrets");
 });
+
 app.get("/submit", (req, res) => {
     res.render("submit");
 });
